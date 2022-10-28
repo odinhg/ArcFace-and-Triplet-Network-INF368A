5fcc7ba5061a499b290a3a0648be112a26520289
odgar2833 <odgar2833@birget.uib.no>
2022-10-28 09:33:53 +0200

    happy with my triplet loss implementation now

--- configs/triplet_loss.py
+++ configs/triplet_loss.py
@@ -1,6 +1,4 @@
 # Settings triplet loss contrastive learning
-#batch_size = 64
-#batch_size = 256 
 batch_size = 128 
 epochs = 25 
 lr = 0.0014
@@ -8,6 +6,6 @@ val = 0.05 #Use 5% for validation data
 test = 0.2 #Use 20% for test data
 image_size = (128, 128)
 
-margin = 0.3 #0.2 # Triplet loss margin
+margin = 0.5 #0.3 #0.2 # Triplet loss margin
 
 model_type = "triplet"
